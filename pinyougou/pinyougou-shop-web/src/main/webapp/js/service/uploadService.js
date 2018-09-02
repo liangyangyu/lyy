a59e42116625f4b8a3acd76dc5d22d1619a1be86
liaojianbin <meinitcasthavefun@foxmail.com>
2018-09-02 12:58:44 +0800

    商品图片上传实现

--- pinyougou/pinyougou-shop-web/src/main/webapp/js/service/uploadService.js
+++ pinyougou/pinyougou-shop-web/src/main/webapp/js/service/uploadService.js
@@ -1,7 +1,9 @@
 app.service("uploadService",function ($http) {
 
     this.uploadFile = function () {
+        //创建表单数据；只能用于html5
         var formData = new FormData();
+        //表单数据中添加file表单项；名称为file，值为在页面中选择的那个文件
         formData.append("file", file.files[0]);
         return $http({
             url:"../upload.do",
