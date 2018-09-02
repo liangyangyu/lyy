d5a7124958ad0c96cfc781086019e892c322289c
liaojianbin <meinitcasthavefun@foxmail.com>
2018-09-02 10:52:33 +0800

    商家管理系统--新增商品基本、描述信息

--- pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/GoodsController.java
+++ pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/GoodsController.java
@@ -3,8 +3,10 @@ package com.pinyougou.shop.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.sellergoods.service.GoodsService;
+import com.pinyougou.vo.Goods;
 import com.pinyougou.vo.PageResult;
 import com.pinyougou.vo.Result;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,10 +29,19 @@ public class GoodsController {
         return goodsService.findPage(page, rows);
     }
 
+    /**
+     * 保存商品基本、描述、sku列表
+     * @param goods 商品信息
+     * @return 操作结果
+     */
     @PostMapping("/add")
-    public Result add(@RequestBody TbGoods goods) {
+    public Result add(@RequestBody Goods goods) {
         try {
-            goodsService.add(goods);
+            //设置商家
+            String sellerId = SecurityContextHolder.getContext().getAuthentication().getName();
+            goods.getGoods().setSellerId(sellerId);
+            goods.getGoods().setAuditStatus("0");//未提交申请审核
+            goodsService.addGoods(goods);
             return Result.ok("增加成功");
         } catch (Exception e) {
             e.printStackTrace();
