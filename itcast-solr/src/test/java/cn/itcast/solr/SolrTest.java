c65814fa5bcd4b46c17837f92826b332dca8cd29
liaojianbin <meinitcasthavefun@foxmail.com>
2018-09-08 10:27:30 +0800

    spring data solr查询

--- itcast-solr/src/test/java/cn/itcast/solr/SolrTest.java
+++ itcast-solr/src/test/java/cn/itcast/solr/SolrTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Criteria;
 import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.ScoredPage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,6 +20,52 @@ public class SolrTest {
 
     @Autowired
     private SolrTemplate solrTemplate;
+
+    //多条件查询
+    @Test
+    public void testMultiQuery(){
+        SimpleQuery query = new SimpleQuery();
+
+        //参数1：在schema.xml文件中对应的编写了的域名；contains表示查询该域包含相应关键字的那些文档是不会分词的；is会分词
+        Criteria criteria1 = new Criteria("item_title").is("魅族");
+        query.addCriteria(criteria1);
+
+        Criteria criteria2 = new Criteria("item_price").greaterThanEqual(500);
+        query.addCriteria(criteria2);
+
+
+        //查询；参数1：查询对象，参数2：是返回结果中的每个文档封装的实体类
+        ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+
+        showMsg(scoredPage);
+    }
+
+    @Test
+    public void testSimpleQuery(){
+        SimpleQuery query = new SimpleQuery("item_title:魅族");
+
+        //设置分页
+        query.setOffset(0);// 起始索引号
+        query.setRows(20);// 本次要查询多少条
+
+        //查询；参数1：查询对象，参数2：是返回结果中的每个文档封装的实体类
+        ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+
+        showMsg(scoredPage);
+    }
+
+    private void showMsg(ScoredPage<TbItem> scoredPage) {
+        System.out.println("总记录数：" + scoredPage.getTotalElements());
+        System.out.println("总页数数：" + scoredPage.getTotalPages());
+
+        for (TbItem item : scoredPage) {
+            System.out.println("id=" + item.getId());
+            System.out.println("title=" + item.getTitle());
+            System.out.println("price=" + item.getPrice());
+            System.out.println("image=" + item.getImage());
+            System.out.println("更新时间=" + item.getUpdateTime());
+        }
+    }
 
     @Test
     public void testDeleteByQuery() {
