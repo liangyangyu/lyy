513f51d2ba4aed5d6be78ad9a5e6927373be196d
liaojianbin <meinitcasthavefun@foxmail.com>
2018-09-14 12:31:00 +0800

    实现用户注册&发送短信验证码

---
+++ pinyougou/pinyougou-user/pinyougou-user-interface/src/main/java/com/pinyougou/user/service/UserService.java
@@ -0,0 +1,16 @@
+package com.pinyougou.user.service;
+
+import com.pinyougou.pojo.TbUser;
+import com.pinyougou.service.BaseService;
+import com.pinyougou.vo.PageResult;
+
+public interface UserService extends BaseService<TbUser> {
+
+    PageResult search(Integer page, Integer rows, TbUser user);
+
+    /**
+     * 发送短信验证码
+     * @param phone 手机号
+     */
+    void sendSmsCode(String phone);
+}
