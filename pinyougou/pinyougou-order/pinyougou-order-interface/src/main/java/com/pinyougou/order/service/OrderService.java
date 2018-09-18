43c2a93dea4bcf91acc70bc78d3f78a53d86a0eb
liaojianbin <meinitcasthavefun@foxmail.com>
2018-09-18 11:24:37 +0800

    搭建订单系统

---
+++ pinyougou/pinyougou-order/pinyougou-order-interface/src/main/java/com/pinyougou/order/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.pinyougou.order.service;
+
+import com.pinyougou.pojo.TbOrder;
+import com.pinyougou.service.BaseService;
+import com.pinyougou.vo.PageResult;
+
+public interface OrderService extends BaseService<TbOrder> {
+
+    PageResult search(Integer page, Integer rows, TbOrder order);
+}
---
+++ pinyougou/pinyougou-order/pinyougou-order-service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,38 @@
+package com.pinyougou.order.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.pinyougou.mapper.OrderMapper;
+import com.pinyougou.pojo.TbOrder;
+import com.pinyougou.order.service.OrderService;
+import com.pinyougou.service.impl.BaseServiceImpl;
+import com.pinyougou.vo.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service(interfaceClass = OrderService.class)
+public class OrderServiceImpl extends BaseServiceImpl<TbOrder> implements OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Override
+    public PageResult search(Integer page, Integer rows, TbOrder order) {
+        PageHelper.startPage(page, rows);
+
+        Example example = new Example(TbOrder.class);
+        Example.Criteria criteria = example.createCriteria();
+        /*if(!StringUtils.isEmpty(order.get***())){
+            criteria.andLike("***", "%" + order.get***() + "%");
+        }*/
+
+        List<TbOrder> list = orderMapper.selectByExample(example);
+        PageInfo<TbOrder> pageInfo = new PageInfo<>(list);
+
+        return new PageResult(pageInfo.getTotal(), pageInfo.getList());
+    }
+}
